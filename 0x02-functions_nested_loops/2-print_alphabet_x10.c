eccb870c4cb4d682640ad097be19d5b1e98827c4
Odunola-Ruby <olawumioladunmoye@gmail.com>
2022-10-21 12:26:01 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,13 +2,14 @@
 
 /**
  * prints_alphabet_x10 - prints 10 times the alphabet, in lowercase
+ * description: written by Ruby
  */
 
-void print_alphabet_x10(void)
+void print_alphabet_x10(void);
 {
 	int count = 0;
 	char letter;
-	
+
 	while (count++ <= 9)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
