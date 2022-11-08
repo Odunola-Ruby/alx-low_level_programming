393d12e49cf9da8b585ecd147ae9f0da8d983233
Odunola-Ruby <olawumioladunmoye@gmail.com>
2022-11-08 10:21:26 -0800

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,17 @@
-/**main - prints its name, followed by a new line.
- * @argc: number of command line arguments.
- * @argv: array that contains the program command line arguments.
- * Return: 0 - success.
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main- a program that prints its nae
+ * @argc: holds the number of arguments passed
+ * @argv: array pointer that holds the arguments passed
+ * Return: Always 0 (Success)
  */
-int main(int __attribute__((__unused__)) argc, char *argv[])
+
+int main(int __attribute__((unused)) argc, char *argv[])
 {
-		printf("%s\n", argv[0]);
-			return (0);
+	int i = 0;
+
+	printf("%s\n", argv[i]);
+	return (0);
 }
